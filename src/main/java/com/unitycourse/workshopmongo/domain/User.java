abaa349518abd84e4fe457378eaaa29885cd8d78
Rafael Cassaro <rafaelcassacosta@hotmail.com>
2023-01-21 19:58:33 -0300

    Referenciando os posts do usuário

--- src/main/java/com/unitycourse/workshopmongo/domain/User.java
+++ src/main/java/com/unitycourse/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.unitycourse.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")//n precisa do collec se n quiser mudar o nome da classe para o banco de dados
@@ -15,11 +18,13 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true) //serve para mostrar os posts so se eles forem acessados
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {}
 
 	
 	public User(String id, String name, String email) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -53,6 +58,15 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -70,6 +84,9 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+
 	
 	
 
