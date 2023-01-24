cbeae634b123eecf361998a7b5e36abeacff5e20
Rafael Cassaro <rafaelcassacosta@hotmail.com>
2023-01-24 01:36:03 -0300

    Consulta simples com query methods

--- src/main/java/com/unitycourse/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/unitycourse/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.unitycourse.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.unitycourse.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//IgnoreCase == ignorar letras maiusculas
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
