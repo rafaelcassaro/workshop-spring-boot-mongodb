a2518feb7f256f22df524277f1b93da438bfb337
Rafael Cassaro <rafaelcassacosta@hotmail.com>
2023-01-24 02:47:30 -0300

    Consulta simples com @Query

--- src/main/java/com/unitycourse/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/unitycourse/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.unitycourse.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.unitycourse.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	//IgnoreCase == ignorar letras maiusculas
 	List<Post> findByTitleContainingIgnoreCase(String text);
